0ed512d723d272ebb399efdc35698718501444f2
Louis-James.Pouleur <louis-james.pouleur@student.ehb.be>
2022-11-16 12:12:36 +0100

    exercise 6

--- exercises/6-animations/6-1 crosshair.js
+++ exercises/6-animations/6-1 crosshair.js
@@ -1,1 +1,23 @@
 "use strict";
+
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
+let width = context.canvas.width;
+let height = context.canvas.height;
+
+window.onmousemove = move;
+
+function move(eventData) {
+	let x = eventData.pageX;
+	let y = eventData.pageY;
+
+	context.fillStyle = "white";
+	context.fillRect(0, 0, width, height);
+
+	context.lineWidth = "5";
+	context.strokeStyle = "red";
+
+	Utils.drawLine(0, y, width, y);
+	Utils.drawLine(x, 0, x, height);
+}
